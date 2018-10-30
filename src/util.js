a86737503b5582ce04e60855cf7c648d9f040f11
JarvisJiang <jiang115jie@gmail.com>
2018-10-30 10:29:25 +0800

    add util.newBuffer

--- src/util.js
+++ src/util.js
@@ -4,5 +4,14 @@ export default {
     emptyArray: [],
     ProtocolError(...args) {
         return new Error(...args);
+    },
+    /**
+     * @description Creates a new buffer
+     * @date 2018-10-30
+     * @param {number|number[]} sizeOrArray buffer size or number array
+     * @returns {Uint8Array}
+     */
+    newBuffer(sizeOrArray) {
+        return new Uint8Array(sizeOrArray);
     }
 };
